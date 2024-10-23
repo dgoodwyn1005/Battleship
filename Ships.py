7108ddfe85e1db7d2353075ac5401af5e2a50bd7
Isaiah Echeverria <iecheverria@gardner-webb.edu>
2024-10-23 17:37:36 -0400

    Can rotate ships and checks if it will be out of bounds or not

--- Ships.py
+++ Ships.py
@@ -8,6 +8,11 @@ class Ships(object):
         self.length = ship_length
         self.sunken = False
         self.file_location = C.IMAGE_FOLDER + "/" + ship_name + ".png"
+        self.head_coordinate = (0, 0)
+        self.rotated = False
+    # add a method to load the ship image? Maybe?
+    # add a coordinate for the ship to replace them on the grid with their orientation
+    # Add a count for the number of hits on the ship
 
     def load_ship_image(self):
         return pygame.image.load(self.file_location)
